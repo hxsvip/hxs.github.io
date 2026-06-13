d919cc45d9e19a934d8cef14a1f0dddd4dccb15c
HxsGit1126 <279985653@qq.com>
2026-06-13 20:57:21 +0800

    Site updated: 2026-06-13 20:57:07

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["2026/06/13/anzhiyu-theme-config/","2026/06/13/hexo-install-guide/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
